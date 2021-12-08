e26714dac890dc6691a46d87b08f45dbc55e3938
yeongminkang <yeongmin.kang@cfind.co.kr>
2021-12-08 19:07:34 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import "./blockchain"
+
+// class Human2 {
+//   public name:string;
+//   public age:number;
+//   public gender:string;
+
+//   constructor(name:string, age:number, gender:string) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+// }
+
+// const ronaldo = new Human2("ronaldo",36,"male");
+
+// interface Human {
+//   name:string,
+//   age:number,
+//   gender:string
+// }
+
+// const person = {
+//   name: "kang",
+//   age: 34,
+//   gender: "male"
+// }
+
+// const sayHi = (person: Human): string => {
+//   return `hello ${person.name}, you are ${person.age}, you ara a ${person.gender}`;
+// }
+
+// const sayHi2 = (person: Human2): string => {
+//   return `hello ${person.name}, you are ${person.age}, you ara a ${person.gender}`;
+// }
+
+export {};
